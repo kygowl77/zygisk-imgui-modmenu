0e505fc787ba3b1b298d50640c40739ef43a5485
kygowl77 <65089017+kygowl77@users.noreply.github.com>
2023-08-19 21:30:33 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -2,7 +2,7 @@
 #define ZYGISK_IMGUI_MODMENU_GAME_H
 
 // TODO: change this
-#define TargetLibName "libUE4.so"
-#define GamePackageName "com.StormingTech.RIPHorrorSurvivalShooter"
+#define TargetLibName "libil2cpp.so"
+#define GamePackageName "com.duoyi.m2m1"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
